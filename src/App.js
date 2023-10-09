8fb96a0a3c622d24fe3606c5ae2faf4a6df08ba5
Todd0554 <toddy9229@outlook.com>
2023-10-09 15:27:30 +1100

    details fixing

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       <Footer />
 
       
-      <a href="" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
+      <a href="#top" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
     </div>
   );
 }
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -12,13 +12,11 @@ function Nav() {
             <div className="navbar-nav ms-auto py-0">
                 <a href="/" className="nav-item nav-link active">Home</a>
                 <a href="/about" className="nav-item nav-link">About</a>
-                <a href="/partnerships" className="nav-item nav-link">Partnerships</a>
-
                 <a href="/course" className="nav-item nav-link">Course</a>
                 <a href="/newsevents" className="nav-item nav-link">News & Events</a>
+                <a href="/partnerships" className="nav-item nav-link">Partnerships</a>
                 {/* <a href="/joinus" className="nav-item nav-link">Join Us</a> */}
                 <a href="/contact" className="nav-item nav-link">Contact</a>
-
                 <a href="/gallery" className="nav-item nav-link">Gallery</a>
             </div>
         </div>
